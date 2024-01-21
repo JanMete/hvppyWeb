6ea46578910ed6d11f2d92bcea13b8cb638bbec0
Jan Metelański <metelanskij@gmail.com>
2024-01-22 00:31:03 +0100

    added photos to artwork

--- src/utils/categories.ts
+++ src/utils/categories.ts
@@ -1,14 +1,17 @@
+import designesThumbnail from '../assets/designesThumbnail.jpg';
+import tattooThumbnail from '../assets/tattooThumbnail.jpeg';
+
 export const categories = [
   {
     categoryName: 'Tattoos',
     patch: '/artwork/tattoos',
-    img: 'https://images.unsplash.com/photo-1611501275019-9b5cda994e8d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    img: tattooThumbnail,
     imgAlt: '',
   },
   {
     categoryName: 'Designes',
     patch: '/artwork/designes',
-    img: 'https://images.unsplash.com/photo-1611501275019-9b5cda994e8d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    img: designesThumbnail,
     imgAlt: '',
   },
   {
--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,44 +1,98 @@
-export const tattoos1 = [
-  {
-    img: 'https://i.imgur.com/UfOysss.jpg',
-  },
-  {
-    img: 'https://i.imgur.com/aKF3mPK.jpg',
-  },
-  {
-    img: 'https://i.imgur.com/HwIiKoN.jpg',
-  },
-];
-export const tattoos2 = [
-  {
-    img: 'https://i.imgur.com/5GqPsCz.jpg',
-  },
-  {
-    img: 'https://i.imgur.com/lO5vY3F.jpg',
-  },
-  {
-    img: 'https://i.imgur.com/oVzTRkU.jpg',
-  },
-];
+import designesPhoto1 from '../assets/Artwork Images/Designes/1.jpg';
+import designesPhoto2 from '../assets/Artwork Images/Designes/2.jpg';
+import designesPhoto3 from '../assets/Artwork Images/Designes/3.jpg';
+import designesPhoto4 from '../assets/Artwork Images/Designes/4.jpg';
+import designesPhoto5 from '../assets/Artwork Images/Designes/5.jpg';
+import designesPhoto6 from '../assets/Artwork Images/Designes/6.jpg';
+import designesPhoto7 from '../assets/Artwork Images/Designes/7.jpg';
+import designesPhoto8 from '../assets/Artwork Images/Designes/8.jpg';
+import designesPhoto9 from '../assets/Artwork Images/Designes/9.jpg';
+import designesPhoto10 from '../assets/Artwork Images/Designes/10.jpg';
+import designesPhoto11 from '../assets/Artwork Images/Designes/11.jpg';
+import designesPhoto12 from '../assets/Artwork Images/Designes/12.jpg';
+import designesPhoto13 from '../assets/Artwork Images/Designes/13.jpg';
+import designesPhoto14 from '../assets/Artwork Images/Designes/14.jpg';
+
+import tattoosPhoto1 from '../assets/Artwork Images/Tattoos/1.jpeg';
+import tattoosPhoto2 from '../assets/Artwork Images/Tattoos/2.jpeg';
+import tattoosPhoto3 from '../assets/Artwork Images/Tattoos/3.jpeg';
+import tattoosPhoto4 from '../assets/Artwork Images/Tattoos/4.jpeg';
+import tattoosPhoto5 from '../assets/Artwork Images/Tattoos/5.jpeg';
+import tattoosPhoto6 from '../assets/Artwork Images/Tattoos/6.jpeg';
+import tattoosPhoto7 from '../assets/Artwork Images/Tattoos/7.jpeg';
+import tattoosPhoto8 from '../assets/Artwork Images/Tattoos/8.jpeg';
+
 export const designes1 = [
   {
-    img: 'https://i.imgur.com/EWH1QR0.jpg',
+    img: designesPhoto1,
   },
   {
-    img: 'https://i.imgur.com/1yqP48e.jpg',
+    img: designesPhoto2,
   },
   {
-    img: 'https://i.imgur.com/XH4EAel.jpg',
+    img: designesPhoto3,
+  },
+  {
+    img: designesPhoto4,
+  },
+  {
+    img: designesPhoto5,
+  },
+  {
+    img: designesPhoto6,
+  },
+  {
+    img: designesPhoto7,
   },
 ];
 export const designes2 = [
   {
-    img: 'https://i.imgur.com/iTy9YVH.jpg',
+    img: designesPhoto8,
   },
   {
-    img: 'https://i.imgur.com/sMjArGY.jpg',
+    img: designesPhoto9,
   },
   {
-    img: 'https://i.imgur.com/oDQHpvy.jpg',
+    img: designesPhoto10,
+  },
+  {
+    img: designesPhoto11,
+  },
+  {
+    img: designesPhoto12,
+  },
+  {
+    img: designesPhoto13,
+  },
+  {
+    img: designesPhoto14,
+  },
+];
+export const tattoos1 = [
+  {
+    img: tattoosPhoto1,
+  },
+  {
+    img: tattoosPhoto2,
+  },
+  {
+    img: tattoosPhoto3,
+  },
+  {
+    img: tattoosPhoto4,
+  },
+];
+export const tattoos2 = [
+  {
+    img: tattoosPhoto5,
+  },
+  {
+    img: tattoosPhoto6,
+  },
+  {
+    img: tattoosPhoto7,
+  },
+  {
+    img: tattoosPhoto8,
   },
 ];
