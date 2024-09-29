653cee796a9c2487315c2ab7f93ae00e3aa5779f
Jan Metelański <metelanskij@gmail.com>
2024-09-29 23:24:02 +0200

    update

--- src/Components/hero/Hero.tsx
+++ src/Components/hero/Hero.tsx
@@ -3,6 +3,7 @@ import style from './hero.module.css';
 import { useTranslation } from 'react-i18next';
 import useIntersectionObserver from '../../hooks/useIntersectionObserver';
 import HEROIMAGE from '../../assets/MainPage/heroMobile.jpeg';
+import { useWindowSizeAndScroll } from '../../hooks/useWindowSizeAndScroll';
 
 export default function Hero() {
   const heroTextRef = useRef<HTMLDivElement>(null);
@@ -15,9 +16,17 @@ export default function Hero() {
     ? 'animate__animated animate__fadeIn'
     : '';
 
+  const { scrollPosition } = useWindowSizeAndScroll();
+  const isHeroImageVisible = scrollPosition < 1200;
+
   return (
     <div className={style.mainContainer}>
-      <img className={style.heroImg} src={HEROIMAGE} />
+      <img
+        className={`${
+          isHeroImageVisible ? style.heroImg : style.staticHeroImg
+        }`}
+        src={HEROIMAGE}
+      />
       <div
         ref={heroTextRef}
         className={`${style.heroContainer} ${animationClass}`}
