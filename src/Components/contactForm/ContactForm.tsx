7aefd4d01558b5f3eb048446ffaa0a6c5f568054
Jan Metelański <metelanskij@gmail.com>
2024-10-02 01:28:37 +0200

    update

--- src/Components/contactForm/ContactForm.tsx
+++ src/Components/contactForm/ContactForm.tsx
@@ -77,7 +77,7 @@ export default function ContactForm() {
         }`}
       >
         {isLoading ? (
-          <LoaderErrorContainer isGallery={false}>
+          <LoaderErrorContainer withBackground={false}>
             <Loader />
           </LoaderErrorContainer>
         ) : (
